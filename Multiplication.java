d9937ef657173f5b08d63228f9fd0c32de871973
Shivaniramesh1304 <shivaniramesh1304@gmail.com>
2025-01-30 21:44:54 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,10 @@
+public class Multiplication{
+    public static void main(String[]args){
+        int a=10;
+        int b=5;
+        int result=a*b;
+        System.out.println("the result of multiplication:"+result);
+
+    }
+    
+}
